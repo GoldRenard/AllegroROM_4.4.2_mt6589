fdbe2a15193bb860a1d92267b928caef2bc82120
Renard Gold <goldrenard@gmail.com>
2014-07-19 18:51:53 +0400

    Clean source

--- _Sources/_Ignore/ContactsProvider/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\Devices\S960_AllegroROM2.0\_Sources\ContactsProvider\bin\AndroidManifest.xml : \
-D:\Devices\S960_AllegroROM2.0\_Sources\ContactsProvider\AndroidManifest.xml \
--- _Sources/_Ignore/ContactsProvider/bin/ContactsProvider-release-unsigned.apk.d
+++
@@ -1,6 +0,0 @@
-D:\Devices\S960_AllegroROM2.0\_Sources\ContactsProvider\bin\ContactsProvider-release-unsigned.apk : \
-D:\Devices\S960_AllegroROM2.0\_Sources\ContactsProvider\bin\ContactsProvider.ap_ \
-D:\Devices\S960_AllegroROM2.0\_Sources\ContactsProvider\bin\classes.dex \
-D:\Devices\S960_AllegroROM2.0\_Sources\ContactsProvider\src\com\android\providers\contacts\EventLogTags.logtags \
-D:\Devices\S960_AllegroROM2.0\_Sources\goldrenard-framework\bin\classes.jar \
-D:\Devices\S960_AllegroROM2.0\_Sources\ContactsProvider\libs\guava-17.0.jar \
